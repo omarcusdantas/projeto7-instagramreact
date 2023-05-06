6ceb7a4a7f051978b2098a1d3280f9e326acad1c
MarcusDantas <marvinsd@gmail.com>
2023-05-06 15:18:14 -0300

    feat: add like and save post features

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,35 @@
+import React from "react"
+
 export default function Post() {
+    const [bookMark, setBookMark] = React.useState("bookmark-outline");
+    const [numberLikes, setNumberLikes] = React.useState(101523);
+    const [likeState, setLikeState] = React.useState({name: "heart-outline", class: ""});
+
+    function toggleSave() {
+        if (bookMark === "bookmark") {
+            setBookMark("bookmark-outline");
+            return;
+        }
+        setBookMark("bookmark");
+    }
+
+    function clickLike() {
+        if (likeState.name !== "heart") {
+            setLikeState({name: "heart", red: "red-heart"});
+            setNumberLikes(numberLikes+1);
+        }
+    }
+
+    function toggleLike() {
+        if (likeState.name === "heart") {
+            setLikeState({name: "heart-outline", red: ""})
+            setNumberLikes(numberLikes-1);
+            return;
+        }
+        setLikeState({name: "heart", red: "red-heart"});
+        setNumberLikes(numberLikes+1);
+    }
+
     return (
         <div className="post">
             <div className="topo">
@@ -12,25 +43,25 @@ export default function Post() {
             </div>
 
             <div className="conteudo">
-                <img src="./assets/img/gato-telefone.svg" alt="gato-telefone"/>
+                <img src="./assets/img/gato-telefone.svg" alt="gato-telefone" onClick={clickLike}/>
             </div>
 
             <div className="fundo">
                 <div className="acoes">
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name={likeState.name} class={likeState.red} onClick={toggleLike}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                        <ion-icon name={bookMark} onClick={toggleSave}></ion-icon>
                     </div>
                 </div>
 
                 <div className="curtidas">
                     <img src="./assets/img/respondeai.svg" alt="respondeai"/>
                     <div className="texto">
-                        Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                        Curtido por <strong>respondeai</strong> e <strong>outras {numberLikes} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,30 @@
+import React from "react"
+
 export default function User() {
+    const [name, setName] = React.useState("catanacomics");
+    const [profilePic, setProfilePic] = React.useState("./assets/img/catanacomics.svg");
+
+    function changeProfilePic() {
+        const newProfilePic = prompt("Novo foto de perfil");
+        if (newProfilePic !== "") {
+            setProfilePic(newProfilePic);
+        }
+    }
+
+    function changeName() {
+        const newName = prompt("Novo nome");
+        if (newName !== "") {
+            setName(newName);
+        }
+    }
+
     return (
         <div className="usuario">
-            <img src="./assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img src={profilePic} alt="imagem de perfil" onClick={changeProfilePic}/>
             <div className="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{name}</strong>
+                    <ion-icon name="pencil" onClick={changeName}></ion-icon>
                 </span>
             </div>
         </div>
